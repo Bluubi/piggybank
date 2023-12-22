1e222a1dfeff4e1dcf07bb0674de95f67c908e1b
BluubiDev <125690321+Bluubi@users.noreply.github.com>
2023-12-22 19:26:54 +0100

    #5 feat: added fonts and images. Decorated index page and added header to main component. (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	test: {
 		environment: 'happy-dom',
+	},
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, './src/assets'),
+		},
 	},
 })
