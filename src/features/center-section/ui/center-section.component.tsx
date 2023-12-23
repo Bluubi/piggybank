bc758816ea33c47a7fea7b492f9e519272d9f56d
BluubiDev <125690321+Bluubi@users.noreply.github.com>
2023-12-23 01:39:01 +0100

    #6 feat: add i18next and start translations (#4)

--- src/features/center-section/ui/center-section.component.tsx
+++ src/features/center-section/ui/center-section.component.tsx
@@ -1,22 +1,25 @@
 import saveMoneyImg from '@assets/finance-woman-cash.png'
 import styles from './center-section.module.css'
 import LinkNavigator from '../../../../lib/link-navigator/link-navigator.component.tsx'
+import {useTranslation} from "react-i18next";
 
 export default function CenterSection() {
+
+	const { t} = useTranslation()
+
 	return (
 		<section className={styles.section}>
 			<div className={styles.content}>
 				{' '}
 				<p>
-					Bienvenido a <b>piggybank, tu propia hucha de ahorros</b>. Apunta tus gastos,
-					ponte límites y olvídate de los molestos cálculos
+					{ t('welcome')}
 				</p>
 				<LinkNavigator
 					path={'/'}
 					theme={'primary'}
 					role={'link'}
 					type={'link'}>
-					<p className={styles.navigator}>Start saving your money </p>
+					<p className={styles.navigator}> { t('start.saving')} </p>
 				</LinkNavigator>
 			</div>
 			<img
