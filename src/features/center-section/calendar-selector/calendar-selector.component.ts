44d4012386e41ce6e56a4c79a3743e87c83371af
BluubiDev <bluubi.dev@gmail.com>
2023-12-26 13:02:33 +0100

    #9feat: add selector format

--- src/features/center-section/calendar-selector/calendar-selector.component.ts
+++ src/features/center-section/calendar-selector/calendar-selector.component.ts
@@ -1,33 +1,40 @@
-export function openDateFormatOption(){
-    const childSelector = document.querySelector('[data-testid="date-selector"]') as HTMLDivElement;
-    console.log(childSelector)
-    const display = childSelector.style.display;
+export function openDateFormatOption() {
+	const childSelector = document.querySelector(
+		'[data-testid="date-selector"]'
+	) as HTMLDivElement
+	console.log(childSelector)
+	const display = childSelector.style.display
 
-    if(display === 'block'){
-        childSelector.style.display = 'none';
-    } else {
-        childSelector.style.display = 'block';
-    }
-
+	if (display === 'block') {
+		childSelector.style.display = 'none'
+	} else {
+		childSelector.style.display = 'block'
+	}
 }
-export function selectDateFormatOption(event: Event){
-    const liElement = event.target as HTMLLIElement;
-    const value = liElement.getAttribute('data-value');
-    console.log(dateFormatOptions.get(value!)!())
-    return dateFormatOptions.get(value!);
+export function selectDateFormatOption(event: Event) {
+	const liElement = event.target as HTMLLIElement
+	const value = liElement.getAttribute('data-value')
+	console.log(dateFormatOptions.get(value!)!())
+	return dateFormatOptions.get(value!)
 }
 
-
-export const dateFormatOptions = new Map<string, () => string>(
-    [["YYYY/MM/DD", () => new Date().toLocaleDateString('es-ES', {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric"
-    })],
-    ["DD/MM/YYYY", () => new Date().toLocaleDateString('zh-Hans-CN', {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric"
-    })]
-    ]
-)
+export const dateFormatOptions = new Map<string, () => string>([
+	[
+		'YYYY/MM/DD',
+		() =>
+			new Date().toLocaleDateString('es-ES', {
+				year: 'numeric',
+				month: 'numeric',
+				day: 'numeric',
+			}),
+	],
+	[
+		'DD/MM/YYYY',
+		() =>
+			new Date().toLocaleDateString('zh-Hans-CN', {
+				day: 'numeric',
+				month: 'numeric',
+				year: 'numeric',
+			}),
+	],
+])
